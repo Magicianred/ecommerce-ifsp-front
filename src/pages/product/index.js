cd39697b7bf95631aca305c590df85ac71f3074e
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2020-09-20 09:57:26 -0300

    Deletion of the paymentForm of the product the paymentForm must be in the order

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -21,7 +21,7 @@ import { handleImage } from '../main/handleImage'
 
 const Product = (props) => {
   const [product, setProduct] = useState({})
-  const [paymentDescription, setPaymentDescription] = useState([])
+  // const [paymentDescription, setPaymentDescription] = useState([])
   const [total, setTotal] = useState()
   const [quantity, setQuantity] = useState(0)
   const [cart, setCart] = useState([])
@@ -29,47 +29,7 @@ const Product = (props) => {
   const loadProduct = async (props) => {
     const { id } = props.match.params
     const { data } = await api.get(`/product?idProduct=${id}`)
-    // let localCart = JSON.parse(localStorage.getItem('cart')
-    // let cartValue = cart.find((item) => item.idProduct === id)
-    // console.log(cartValue)
     setProduct(data[0])
-  }
-
-  // const loadQuantity = () => {
-  //   const { id } = props.match.params
-  //   //create a copy of our cart state, avoid overwritting existing state
-  //   let cartCopy = [...cart]
-
-  //   //look for item in cart array
-  //   let existentItem = cartCopy.find((cartItem) => cartItem.idProduct == id)
-
-  //   if (!existentItem) return
-
-  //   if (existentItem.qtdd <= 0) {
-  //     //remove item  by filtering it from cart array
-  //     cartCopy = cartCopy.filter((item) => item.idProduct != { id })
-  //   }
-  //   console.log('existentItem')
-  //   // setQuantity(existentItem.qtdd)
-  //   setCart(cartCopy)
-
-  //   let cartString = JSON.stringify(cartCopy)
-  //   localStorage.setItem('cart', cartString)
-  // }
-
-  //paymentForm
-  const loadPaymentForm = async () => {
-    const { data } = await api.get('/paymentForm')
-    setPaymentDescription(data)
-  }
-
-  //list the forms of payment
-  const handlePayment = ({ paymentForm, idPaymentForm }) => {
-    return (
-      <option key={idPaymentForm} value={paymentForm}>
-        {paymentForm}
-      </option>
-    )
   }
 
   //The addition or subtraction of one product alter the total
@@ -119,12 +79,8 @@ const Product = (props) => {
 
   useEffect(() => {
     loadProduct(props)
-    loadPaymentForm()
-    // loadQuantity()
     if (localStorage.getItem('cart'))
       setCart(JSON.parse(localStorage.getItem('cart')))
-    // localCart = JSON.parse(localCart)
-    //load persisted cart into state if it exists
     if (localCart) localStorage.setItem('cart', localCart)
   }, [product, props])
 
@@ -169,12 +125,6 @@ const Product = (props) => {
                       onChange={(e) => handleChange(e)}
                     />
                   </StyledLabel>
-                </DivRow>
-                <DivRow>
-                  <select>
-                    <option value="-">Formas de Pagamento</option>
-                    {paymentDescription.map(handlePayment)}
-                  </select>
                 </DivRow>
               </div>
               <div>
